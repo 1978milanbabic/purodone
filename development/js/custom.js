030bdee89e1f668c5cca42ea357400b9a4fa6327
Your NameMilan <1978milan.babic@gmail.com>
2020-04-05 08:19:10 +0200

    [section1] s1 started, anim needs correction

--- development/js/custom.js
+++ development/js/custom.js
@@ -29,22 +29,58 @@
                     // top
                     "top/logo_top.png",
                     "top/stomack.jpg",
+                    // leafs
+                    "leafs/leaf1.png",
+                    "leafs/leaf2.png",
+                    "leafs/leaf3.png",
+                    // order
+                    "order/bottle1.jpg",
+                    "order/bottle3.jpg",
+                    "order/bottle5.jpg",
+                    "order/free.png",
                     // section 2
-                    "section2/leaf1.png",
-                    "section2/leaf1.png",
-                    "section2/leaf2.png",
-                    "section2/leaf3.png",
+                    "section2/leaves.png",
+                    // section 4
+                    "section4/slide_1.jpg",
+                    "section4/slide_2.jpg",
+                    "section4/slide_3.jpg",
+                    "section4/slide_4.jpg",
+                    "section4/slide_5.jpg",
+                    "section4/slide_6.jpg",
+                    // section10
+                    "section10/img1.jpg",
+                    "section10/img2.jpg",
+                    "section10/img3.jpg"
                 ],
                 1: [
                     // top
                     "top/daske.jpg",
                     "top/top_big_bottle.png",
-                    "top/top_bottles.png",
                     "top/top.png",
-                    // section 2
-                    "section2/bottle.png",
+                    // bottle
+                    "bottle/bottle.png",
+                    "bottle/bottles.png",
+                    "bottle/top_bottles.png",
                     // section 3
-
+                    "section3/bgd.jpg",
+                    // section 5
+                    "section5/bgd.jpg",
+                    // section 6
+                    "section6/checkmark.png",
+                    // section 7
+                    "section7/medic.png",
+                    // section 8
+                    "section8/bgd.png",
+                    "section8/bottle.png",
+                    // section 9
+                    "section9/bgd.jpg",
+                    "section9/bottle.png",
+                    // section 11
+                    "section11/girl.jpg",
+                    // section 12
+                    "section12/bgd.png",
+                    // bottom
+                    "bottom/bgd.png"
                 ],
                 2: [""],
                 3: [""]
@@ -81,6 +117,7 @@
                     $("#pics-preload").append(aPic);
                 }
                 this.checkLoad();
+                this.setImages(size);
             },
             checkLoad: function () {
                 var t = this;
@@ -102,6 +139,34 @@
             },
             successLoad: function () {
                 $(".loader").fadeOut(500);
+            },
+            setImages: function (size) {
+                var mode;
+                switch (size) {
+                    case 1:
+                    case "1":
+                        mode = "desk";
+                        break;
+                    case 2:
+                    case "2":
+                        mode = "tablet";
+                        break;
+                    case 3:
+                    case "3":
+                        mode = "mob";
+                        break;
+                }
+                function setBgdAll(el, route) {
+                    $(el).css("background-image", "url(./img/all" + route + ")");
+                }
+                function setBgd(el, route) {
+                    $(el).css("background-image", "url(./img/" + mode + "/" + route + ")");
+                }
+
+
+
+                setBgd("header", "top/daske.jpg");
+                setBgd("section.section1", "top/top.png")
             }
         };
 
